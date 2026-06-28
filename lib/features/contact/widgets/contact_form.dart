0e97476074956680d3f4a8a7310bfe7632faafc5
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-06-28 07:45:23 +0630

    Update lib/features/contact/widgets/contact_form.dart

--- lib/features/contact/widgets/contact_form.dart
+++ lib/features/contact/widgets/contact_form.dart
@@ -1,2 +1,2 @@
-
-
+import 'package:flutter/material.dart';
+class ContactForm extends StatelessWidget {  const ContactForm({super.key});  @override  Widget build(BuildContext context) => const SizedBox.shrink();}
