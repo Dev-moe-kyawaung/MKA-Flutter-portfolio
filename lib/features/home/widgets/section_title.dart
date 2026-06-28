27c6ed80240077cf82d7f1fcde6840bb8b9f6dec
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-06-28 07:45:20 +0630

    Update lib/features/home/widgets/section_title.dart

--- lib/features/home/widgets/section_title.dart
+++ lib/features/home/widgets/section_title.dart
@@ -1,2 +1,2 @@
-
-
+import 'package:flutter/material.dart';
+class SectionTitle extends StatelessWidget {  final String title;  final String subtitle;  const SectionTitle({    super.key,    required this.title,    required this.subtitle,  });  @override  Widget build(BuildContext context) {    return Column(      crossAxisAlignment: CrossAxisAlignment.start,      children: [        Text(title, style: const TextStyle(fontSize: 34, fontWeight: FontWeight.w800)),        const SizedBox(height: 10),        Text(subtitle, style: const TextStyle(height: 1.6)),      ],    );  }}
