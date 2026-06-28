d7af6d1112809a78e00520288ba84ad3f7fc04d9
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-06-28 08:07:02 +0630

    Create section_keys.dart

---
+++ lib/core/utils/section_keys.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SectionKeys {
+  static final home = GlobalKey();
+  static final hero = GlobalKey();
+  static final about = GlobalKey();
+  static final skills = GlobalKey();
+  static final services = GlobalKey();
+  static final projects = GlobalKey();
+  static final testimonials = GlobalKey();
+  static final contact = GlobalKey();
+}
