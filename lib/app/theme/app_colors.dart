024fa08f0bef079011aae33b6641b0b9ca0cd051
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-06-28 08:09:17 +0630

    Create app_colors.dart

---
+++ lib/app/theme/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const background = Color(0xFF0B0F19);
+  static const surface = Color(0xFF111827);
+  static const surfaceAlt = Color(0xFF1F2937);
+  static const primary = Color(0xFF8B5CF6);
+  static const secondary = Color(0xFF14B8A6);
+  static const text = Color(0xFFF8FAFC);
+  static const mutedText = Color(0xFF94A3B8);
+  static const border = Color(0xFF243041);
+}
