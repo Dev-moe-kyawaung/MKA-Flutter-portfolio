c5f9750370c8b1b50bd68c95e28d2cb0e4f9e612
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-06-28 08:15:24 +0630

    Create testimonial_model.dart

---
+++ lib/data/models/testimonial_model.dart
@@ -0,0 +1,11 @@
+class TestimonialModel {
+  final String name;
+  final String role;
+  final String message;
+
+  const TestimonialModel({
+    required this.name,
+    required this.role,
+    required this.message,
+  });
+}
