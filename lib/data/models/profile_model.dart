4cb2ebbc1dca58c9384dcda6ddf8fa1e3101104a
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-06-28 08:14:03 +0630

    Create profile_model.dart

---
+++ lib/data/models/profile_model.dart
@@ -0,0 +1,31 @@
+class ProfileModel {
+  final String name;
+  final String shortName;
+  final String title;
+  final String subtitle;
+  final String experience;
+  final String location;
+  final String email;
+  final String github;
+  final String linkedin;
+  final String telegram;
+  final String instagram;
+  final String playStore;
+  final String avatarUrl;
+
+  const ProfileModel({
+    required this.name,
+    required this.shortName,
+    required this.title,
+    required this.subtitle,
+    required this.experience,
+    required this.location,
+    required this.email,
+    required this.github,
+    required this.linkedin,
+    required this.telegram,
+    required this.instagram,
+    required this.playStore,
+    required this.avatarUrl,
+  });
+}
